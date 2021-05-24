2c2cf9d9321d7fee2eca26606eaa8e39ce7f33d3
cusbert <mindychoi20@gmail.com>
2021-05-24 23:04:03 +0900

    feat: 상속관계매핑 - 단일테이블 전략, 구현클래스마다 테이블 전략

--- src/main/java/com/jpa/study/ch7/Item.java
+++ src/main/java/com/jpa/study/ch7/Item.java
@@ -3,7 +3,9 @@ package com.jpa.study.ch7;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) // 매핑전략을 join 전략으로 지정
+// @Inheritance(strategy = InheritanceType.JOINED) // 매핑전략을 join 전략으로 지정
+// @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일 테이블 전략
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 구현 클래스마다 테이블 전략
 @DiscriminatorColumn(name = "DTYPE") // 부모 클래스에 구분 컬럼 지정
 public abstract class Item {
 
