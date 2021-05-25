9a1e86a2a6d22d4682afabda354bfe6ebf50eeb1
cusbert <mindychoi20@gmail.com>
2021-05-25 21:59:42 +0900

    feat: MappedSuperclass

--- src/main/java/com/jpa/study/ch7/BaseEntity.java
+++ src/main/java/com/jpa/study/ch7/BaseEntity.java
@@ -1,16 +1,19 @@
 package com.jpa.study.ch7;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.MappedSuperclass;
+import java.util.Date;
 
 // 매핑 정보를 상속할 목적으로 사용함, 부모 테이블과 매핑 X
 // 공통 매핑 정보를 정의하여 상속
 @MappedSuperclass
+@Getter
+@Setter
 public abstract class BaseEntity {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String name;
+    private Date createdDate;
+    private Date modifiedDate;
+
 }
--- src/main/java/com/jpa/study/ch7/Member.java
+++ src/main/java/com/jpa/study/ch7/Member.java
@@ -1,11 +1,19 @@
 package com.jpa.study.ch7;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Member extends BaseEntity {
 
-    // ID 상속
-    // NAME 상속
-    private String email;
+    @Id
+    @GeneratedValue
+    @Column(name="MEMBER_ID", nullable = false)
+    private Long id;
+
+    @Column(name="USER_NAME", nullable = false)
+    private String username;
+
 }
