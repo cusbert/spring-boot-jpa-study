cff6f3b5619d4b17cf79c7723a501d8a2ede54d5
cusbert <mindychoi20@gmail.com>
2021-06-11 23:37:51 +0900

    feat: Add constructor

--- src/main/java/com/jpa/shop/domain/Address.java
+++ src/main/java/com/jpa/shop/domain/Address.java
@@ -1,13 +1,18 @@
 package com.jpa.shop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     private String city;
     private String street;
     private String zipCode;
+
 }
