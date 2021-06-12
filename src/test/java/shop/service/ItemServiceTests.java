85c7505c9aa1baccaf3ae0b2a360c15deda21434
cusbert <mindychoi20@gmail.com>
2021-06-12 20:29:20 +0900

    test: Add item, order service test

---
+++ src/test/java/shop/service/ItemServiceTests.java
@@ -0,0 +1,65 @@
+package shop.service;
+
+import com.jpa.shop.JpaStudyApplication;
+import com.jpa.shop.domain.Album;
+import com.jpa.shop.domain.Item;
+import com.jpa.shop.repository.ItemRepository;
+import com.jpa.shop.service.ItemService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = JpaStudyApplication.class)
+public class ItemServiceTests {
+
+    @Autowired
+    ItemService itemService;
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    public void saveTest() {
+        // given
+        Album item = new Album();
+        item.setId(1l);
+        item.setStockQuantity(1);
+        item.setDirector("Steve");
+
+        // when
+        itemService.saveItem(item);
+
+        // then
+        Album findItem = (Album) itemRepository.findOne(1l);
+
+        assertEquals(item.getId(), findItem.getId());
+        assertEquals(item.getDirector(), findItem.getDirector());
+    }
+
+    @Test
+    public void findAllTest() {
+        // given
+        Item item1 = new Album();
+        item1.setName("album1");
+
+        Item item2 = new Album();
+        item2.setName("album2");
+
+        // when
+        itemService.saveItem(item1);
+        itemService.saveItem(item2);
+        List<Item> items = itemService.findItems();
+
+        assertEquals(2, items.size());
+
+    }
+
+
+}
---
+++ src/test/java/shop/service/OrderServiceTests.java
@@ -0,0 +1,118 @@
+package shop.service;
+
+import com.jpa.shop.JpaStudyApplication;
+import com.jpa.shop.domain.*;
+import com.jpa.shop.exception.NotEnoughStockException;
+import com.jpa.shop.repository.OrderRepository;
+import com.jpa.shop.service.OrderService;
+import org.hibernate.jdbc.Expectations;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = JpaStudyApplication.class)
+public class OrderServiceTests {
+
+    // 주문 성공 해야함
+    // 주문 시 재고 수량 초과 하면 안됨
+    // 주문 취소 성공
+
+    @PersistenceContext
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    @Transactional
+    @DisplayName("상품 주문 성공")
+    public void success_order_item() throws Exception {
+
+        int stockQuantity = 10;
+        int orderItemPrice = 9900;
+        // Given
+        Member member = createMember();
+        Item item = createBook("JPA Book", orderItemPrice, stockQuantity);
+        int orderCount = 2;
+
+        // When
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        // Then
+        Order getOrder = orderRepository.findOne(orderId);
+        Assertions.assertEquals(OrderStatus.ORDER, getOrder.getOrderStatus());
+        Assertions.assertEquals(1, getOrder.getOrderItems().size()); // 주문한 상품 종류 수
+        Assertions.assertEquals(orderItemPrice * orderCount, getOrder.getTotalPrice());
+        Assertions.assertEquals(stockQuantity - orderCount, item.getStockQuantity());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("상품주문 실패 - 재고수량초과")
+    public void fail_order_stock_quantity_over() throws Exception {
+
+        int stockQuantity = 5;
+        int orderItemPrice = 9900;
+        // Given
+        Member member = createMember();
+        Item item = createBook("JPA Book", orderItemPrice, stockQuantity);
+        int orderCount = 10;
+
+        Assertions.assertThrows(NotEnoughStockException.class, () -> {
+            Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+        });
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("주문 취소")
+    public void success_order_cancel() throws Exception {
+
+        int stockQuantity = 10;
+        int orderItemPrice = 9900;
+        // Given
+        Member member = createMember();
+        Item item = createBook("JPA Book", orderItemPrice, stockQuantity);
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //When
+        orderService.cancelOrder(orderId);
+
+        //Then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        Assertions.assertEquals(OrderStatus.CANCEL, getOrder.getOrderStatus());
+        Assertions.assertEquals(stockQuantity, item.getStockQuantity()); // 취소한 만큼 수량 복구
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setUsername("Chris pine");
+        member.setAddress(new Address("seoul", "city", "123-123"));
+        em.persist(member);
+
+        return member;
+    }
+
+    private Item createBook(String title, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(title);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+
+        em.persist(book);
+        return book;
+    }
+}
