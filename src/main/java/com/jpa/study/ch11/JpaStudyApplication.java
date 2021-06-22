02593145f9cbf1a1dc6449963fcbff9ce03b7cfc
cusbert <mindychoi20@gmail.com>
2021-06-22 23:06:38 +0900

    설정 테스트

--- src/main/java/com/jpa/study/ch11/JpaStudyApplication.java
+++ src/main/java/com/jpa/study/ch11/JpaStudyApplication.java
@@ -2,8 +2,14 @@ package com.jpa.study.ch11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// @SpringBootApplication
+@SpringBootApplication
+@ComponentScan("com.jpa.study.ch11")
+@EntityScan("com.jpa.study.ch11")
+@EnableJpaRepositories("com.jpa.study.ch11")
 public class JpaStudyApplication {
 
 	public static void main(String[] args) {
